fdb76ec9c8ba3036ca7460dcf338bb60d277c4e1
David Solis <doze.inept.0n@icloud.com>
2022-09-20 09:23:41 -0700

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by David on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
